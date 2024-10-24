d770f458a767f43a5f4bf4188882f0764344b2f5
Mateusz Zawadzki <matizawadzki@interia.pl>
2024-10-24 20:03:30 +0200

    First Commit

---
+++ Labirynt/MapSite.java
@@ -0,0 +1,32 @@
+import java.awt.Image;
+
+enum Directions {
+    NORTH, SOUTH, EAST, WEST
+};
+
+public abstract class MapSite {
+    private int x, y; // wspolrzedne
+    public final static int lenght = 50; // rozmiar sciany
+
+    public MapSite(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Rysuje element labiryntu
+     * 
+     * @param image - obiekt na którym rysujemy
+     */
+    public void draw(Image image) {
+
+    }
+}
